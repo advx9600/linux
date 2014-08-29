fa1758ddfe3bc1b1acbf38183f3fd5aaa9a5732e
abc <abc@abc-To-be-filled-by-O-E-M.(none)>
2014-08-29 11:50:41 -0400

    fastboot

--- shell/fastboot.sh
+++ shell/fastboot.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
 type=$1
-android_out=android4.0_realv210_ver_1_0/out/target/product/smdkv210
+
+total_out=/home/abc/Public/android/android4.0/s5pv210
+uboot_out=$total_out/u-boot-samsung-dev
+kernel_out=$total_out/kernel
+android_out=$total_out/android4.0_realv210_ver_1_0/out/target/product/smdkv210
+fast_boot="sudo /home/abc/Public/tools/android-sdk/android-sdk-linux/platform-tools/fastboot"
+
 if [ $type = "bootloader" ]
 then
-	fastboot flash bootloader u-boot-samsung-dev/u-boot.bin
+	$fast_boot flash bootloader u-boot-samsung-dev/u-boot.bin
 elif [ $type = "kernel" ]
 then
-	fastboot flash kernel kernel/arch/arm/boot/zImage
+	$fast_boot flash kernel kernel/arch/arm/boot/zImage
 elif [ $type = "ramdisk" ]
 then
-	fastboot flash ramdisk $android_out/ramdisk-yaffs.img
+	$fast_boot flash ramdisk $android_out/ramdisk-yaffs.img
 elif [ $type = "system" ]
 then
-	fastboot flash system $android_out/system.img
+	$fast_boot flash system $android_out/system.img
 elif [ $type = "userdata" ]
 then
-	fastboot flash userdata $android_out/userdata.img
+	$fast_boot flash userdata $android_out/userdata.img
 fi
