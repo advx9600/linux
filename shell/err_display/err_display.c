cb889f4c0f22359cbfcfefaaf2a1b4e69fcf3e35
abc <abc@abc-virtual-machine.(none)>
2015-04-29 14:04:55 +0800

    for make command  display err

---
+++ shell/err_display/err_display.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main()
+{
+
+    const char* file = "/home/abc/Public/test/save.txt";
+    char buf[1024];
+    int len;
+
+    FILE* fd = freopen(file,"w",stderr);
+    if (fd == NULL){perror("freopen");return -1;}
+
+    system("make");
+
+    fclose(fd);
+
+    fd = fopen(file,"r");
+    if (fd == NULL) {perror("fopen");return -1;}
+
+    while( !feof(fd) ){
+	if (fgets(buf,sizeof(buf),fd)){
+	    buf[sizeof(buf)-2]=0;
+	    if (strstr(buf,"error"))
+		printf("\033[31m""%s""\033[m", buf);
+	}
+    }
+
+    fclose(fd);
+
+    return 0;
+}
