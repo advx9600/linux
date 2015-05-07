24860f236291dd1540416cad79255413363f96f5
abc <abc@abc-virtual-machine.(none)>
2015-05-07 10:03:46 +0800

    增加U盘功能,但只能Ubuntu下工作正常

--- tools/adb/adbd/usb_config.sh
+++ tools/adb/adbd/usb_config.sh
@@ -1,5 +1,6 @@
 echo 0 > /sys/class/android_usb/android0/enable
 echo 18d1 >/sys/class/android_usb/android0/idVendor
 echo D002 > /sys/class/android_usb/android0/idProduct
-echo adb > /sys/class/android_usb/android0/functions
+echo mass_storage,adb > /sys/class/android_usb/android0/functions
+echo "/dev/sdcard" >/sys/class/android_usb/android0/f_mass_storage/lun/file
 echo 1 > /sys/class/android_usb/android0/enable
