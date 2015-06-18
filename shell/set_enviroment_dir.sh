d795890e5efd17d60f87ea73bac34c2c5560c65c
abc <nwz88348307@163.com>
2015-06-18 17:26:49 +0800

    a

--- shell/set_enviroment_dir.sh
+++ shell/set_enviroment_dir.sh
@@ -2,6 +2,7 @@ myDirs="/home/abc/Public/tools/ffmpeg/_install
         "
 setEnvs="LD_LIBRARY_PATH=lib
 	C_INCLUDE_PATH=include
+	CPLUS_INCLUDE_PATH=include
 	PKG_CONFIG_PATH=lib/pkgconfig
 	XDG_DATA_DIRS=share
 	PATH=bin
