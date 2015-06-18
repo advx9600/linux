62737ef754c1bc7434ccf0a544eb5878a23db431
abc <nwz88348307@163.com>
2015-06-18 16:33:15 +0800

    set enviroment path script

---
+++ shell/set_enviroment_dir.sh
@@ -0,0 +1,24 @@
+myDirs="/home/abc/Public/tools/ffmpeg/_install
+        "
+setEnvs="LD_LIBRARY_PATH=lib
+	C_INCLUDE_PATH=include
+	PKG_CONFIG_PATH=lib/pkgconfig
+	XDG_DATA_DIRS=share
+	PATH=bin
+	"
+
+for i in $setEnvs
+do
+for j in $myDirs
+do
+	arr=(${i//=/ })
+	valName=${arr[0]}
+	valPath=$j"/"${arr[1]}
+	eval k=\$$valName
+	if [ "$k" = "" ];then
+		export $valName=$valPath
+	else
+		export $valName=$k:$valPath
+	fi
+done
+done
